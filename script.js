68c4c90d48bf685965b0ec91b0014803e06f948e
joe san <153516903+JoeSanGH@users.noreply.github.com>
2024-05-17 14:02:21 +0200

    Add layout, style and functionality.

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
-const quotes = [
-    "When you arise in the morning, think of what a precious privilege it is to be alive, to breathe, to think, to enjoy, to love. Then make that day count by setting your mind on doing good.",
-    "It is no measure of health to be well-adjusted to a profoundly sick society. A profound individual and collective transformation is required, but that cannot come about if we are merely adjusting ourselves to the existing norms.",
-    "The secret of change is to focus all of your energy not on fighting the old, but on building the new." 
-];
+let fortunes = ['When you arise in the morning, think of what a precious privilege it is to be alive, to breathe, to think, to enjoy, to love. Then make that day count by setting your mind on doing good. - Marcus Aurelius',
+  'It is no measure of health to be well-adjusted to a profoundly sick society. A profound individual and collective transformation is required, but that cannot come about if we are merely adjusting ourselves to the existing norms. - Jiddu Krishnamurti',
+  'The secret of change is to focus all of your energy not on fighting the old, but on building the new. - Socrates',
+]
 
-const randomQuote = () => {
-    const randomIndex = Math.floor(Math.random() * quotes.length);
-    return quotes[randomIndex];
-};
+let button = document.getElementById('fortuneButton');
+let fortune = document.getElementById('fortune');
 
-console.log(randomQuote());
+function fortuneSelector(){
+  let randomFortune = Math.floor(Math.random() * fortunes.length);
+  return fortunes[randomFortune];
+}
+
+function showFortune(){
+  fortune.innerHTML = fortuneSelector();
+  button.innerHTML = 'Come back tomorrow!';
+  button.style.cursor = 'default';
+  // button.removeEventListener('click', showFortune);
+}
+
+button.addEventListener('click', showFortune);
